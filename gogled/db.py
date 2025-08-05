62898fcde9793c76fd298f2360a6022784163ece
Bender666 <138517045+Gergiy1203@users.noreply.github.com>
2025-08-05 22:17:24 +0300

    Update db.py

--- gogled/db.py
+++ gogled/db.py
@@ -1,4 +1,10 @@
 import main
 import sqlite3 as sq
 
-conction = sq.connect('your_database_db')
+conction = sqlite3.connect('databse.sqlite')
+
+
+
+cursor = connect.cursor()
+cursor.execute("SELECT <column_name> FROM <table_name>")
+results = cursor.fetchall()
