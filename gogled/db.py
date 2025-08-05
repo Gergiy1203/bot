53b0786bab4318e0deb5f5b7b37d60e0abb291e8
Bender666 <138517045+Gergiy1203@users.noreply.github.com>
2025-08-05 22:20:14 +0300

    Update db.py

--- gogled/db.py
+++ gogled/db.py
@@ -8,3 +8,6 @@ conction = sqlite3.connect('databse.sqlite')
 cursor = connect.cursor()
 cursor.execute("SELECT <column_name> FROM <table_name>")
 results = cursor.fetchall()
+cursor.execute("INSERT INTO <table_name> (id, name, surname, birthdate) VALUES (2, 'Petr', 'Ivanov', '2003-12-13') ")
+connect.commit()
+
