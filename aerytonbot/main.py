87228c7a6df9421a4ae3f18455fc6029cabcb95b
Bender666 <138517045+Gergiy1203@users.noreply.github.com>
2026-03-22 18:17:29 +0300

    Update main.py

--- aerytonbot/main.py
+++ aerytonbot/main.py
@@ -12,3 +12,4 @@ async def main():
         
 
 asyncio.run(main())
+ERROR: Could not install packages due to an OSError: HTTPSConnectionPool(host='files.pythonhosted.org', port=443): Max retries exceeded with url: /packages/de/f0/c81e05b613866b76d2d1066490adf1a3dbc4ee9d9c839961c3fc8a6997af/pip-26.0.1-py3-none-any.whl.metadata (Caused by ReadTimeoutError("HTTPSConnectionPool(host='files.pythonhosted.org', port=443): Read timed out. (read timeout=15)"))
